be069516c91a07df88d1368c40dc988d7d0ae95b
Bohdan Makohin <bm@loopbycoop.com>
2020-11-29 22:13:42 +0200

    Make Data Scientists of Mars happy! (exercise 1)

---
+++ Program.fs
@@ -0,0 +1,41 @@
+open System
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Giraffe
+open Fake.Core
+open Microsoft.AspNetCore.Http
+
+let predict inputStream =
+    let resultPrefix = "result: "
+
+    CreateProcess.fromRawCommandLine "python" "./PythonModel/model1.py"
+    |> CreateProcess.withStandardInput (UseStream (false, inputStream))
+    |> CreateProcess.redirectOutput
+    |> CreateProcess.map (fun processResult -> processResult.Result.Output)
+    |> Proc.run
+    |> String.splitStr Environment.NewLine
+    |> Seq.find (fun line -> line.StartsWith(resultPrefix))
+    |> String.replaceFirst resultPrefix String.Empty
+
+let handlePredictionRequest (context : HttpContext) =
+    let prediction = predict context.Request.Body
+
+    context.SetContentType ("application/json")
+    context.WriteStringAsync prediction
+
+let webApp =
+    route "/predict" >=> handleContext(handlePredictionRequest)
+
+[<EntryPoint>]
+let main _ =
+    Host.CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    .UseUrls("http://localhost:8080/")
+                    .Configure(fun app -> app.UseGiraffe webApp)
+                    .ConfigureServices(fun services -> services.AddGiraffe() |> ignore)
+                    |> ignore)
+        .Build()
+        .Run()
+    0
